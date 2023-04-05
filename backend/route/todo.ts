cc945d9e8288893b7ff7b328d252580443656c8a
Few <odds@Few.local>
2023-04-05 09:28:48 +0700

    fix: try catch method post

--- backend/route/todo.ts
+++ backend/route/todo.ts
@@ -6,19 +6,20 @@ const route = Router();
 route.get("/todo", async (_req: Request, res: Response) => {
     try {
        const data = (await Todo.find({}));
-
         res.status(200).send(data);
     } catch (error) {
-        res.status(500).send("error");
+        res.status(500).send(error);
     }
 });
 
 route.post('/todo', async (_req: Request, res: Response) => {
     const data = new Todo({title: 'work 1', status: 'Done'});
-    
-    await data.save();
-
-    res.send("todo post")
+    try {
+        await data.save();
+        res.send(200).send("save success");
+    } catch (error) {
+        res.status(500).send(error)
+    }
 })
 
 export {route};
